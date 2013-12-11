315c751920613938ad1b2c6dff0dc5e72f2666d4
niko <nikomomo@gmail.com>
2013-12-11 12:42:14 +0100

    tidy

--- miniko.js
+++ miniko.js
@@ -170,7 +170,6 @@ if content is provided, all matching element will have it.
 	};
 
 	W.ready = function(cb){
-		console.log('ready state = '+D.readyState);
 		if(/complete|loaded|interactive/.test(D.readyState)) cb();
 		else if(D.attachEvent) D.attachEvent('onD.eady',cb()); 
 		else D.addEventListener('DOMContentLoaded',function(){cb()}, false);
